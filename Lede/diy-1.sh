bff52372216d37d72425789abc52741e230dbae5
svgr110 <70746911+svgr110@users.noreply.github.com>
2020-11-18 16:53:03 +0800

    Update diy-1.sh

--- Lede/diy-1.sh
+++ Lede/diy-1.sh
@@ -3,7 +3,7 @@
 #
 
 # 使用源码自带ShadowSocksR Plus+出国软件
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge  #主题-edge-动态登陆界面
